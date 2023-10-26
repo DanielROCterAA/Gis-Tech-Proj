fc899ccac3818ccd99c8b7429c29cd236fced9bd
DanielRocTeraa <86284@roc-teraa.nl>
2023-10-26 11:04:48 +0200

    Arduino files added

---
+++ Arduino/ArduinoPizzaBaker/ArduinoPizzaBaker/ArduinoPizzaBaker.py
@@ -0,0 +1,24 @@
+
+import time
+import sys
+from fhict_cb_01.custom_telemetrix import CustomTelemetrix
+
+
+def setup():
+    global board
+    board = CustomTelemetrix(com_port = "COM8")
+
+def loop():
+
+    #Code here
+    pass
+
+setup()
+while True:
+        try:
+            loop()
+        except KeyboardInterrupt:
+            print("Exiting")
+            board.shutdown()
+            sys.exit()
+    
